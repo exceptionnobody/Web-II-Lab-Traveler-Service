4563a951dc760ed57f0dbb100866552cae69ed9f
Marco Ballario <marco.ballario1997@gmail.com>
2022-05-04 16:36:59 +0200

    Create new Spring Boot project

---
+++ src/main/kotlin/it/polito/wa2/g12/travelerservice/Wa2G12TravelerServiceApplication.kt
@@ -0,0 +1,11 @@
+package it.polito.wa2.g12.travelerservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Wa2G12TravelerServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<Wa2G12TravelerServiceApplication>(*args)
+}
