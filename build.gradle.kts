1dc978c3e5e86c8af73a7223e9b97699a0966ab4
Marco Ballario <marco.ballario1997@gmail.com>
2022-05-17 19:24:01 +0200

    Security (#2)
    * to be modified
    
    * adding first configuration security, must be modified
    
    * adding cookie jwt, but this work is for testing
    
    * adding role, user, security api and RBAC api
    
    * bug correction: now it checks only the roles in the jwt and not in the db. Added iat time in the jwt as the specification says
    
    * bug correction
    
    * Set correct project properties
    
    * Make the traveler service work with the login service
    
    * Comment useless code
    
    * add class JwtUtils and corrections
    
    * changed details roles
    
    * point 9 and 10 should be completed
    
    * Clean code
    
    Co-authored-by: Francesco Galazzo <72932124+exceptionnobody@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,12 +18,22 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.arrow-kt:arrow-core:1.0.1")
+
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 }
 
 tasks.withType<KotlinCompile> {
