672a4e956a7b3b701680fcb4137e8c5421f4d7f4
Marco Ballario <marco.ballario1997@gmail.com>
2022-06-03 13:36:00 +0200

    Update db initialization file

--- init/create_dbs.sql
+++ init/create_dbs.sql
@@ -2,3 +2,7 @@ CREATE DATABASE login_db;
 GRANT ALL PRIVILEGES ON DATABASE login_db TO postgres;
 CREATE DATABASE traveler_db;
 GRANT ALL PRIVILEGES ON DATABASE traveler_db TO postgres;
+CREATE DATABASE ticket_catalogue_db;
+GRANT ALL PRIVILEGES ON DATABASE ticket_catalogue_db TO postgres;
+CREATE DATABASE payment_db;
+GRANT ALL PRIVILEGES ON DATABASE payment_db TO postgres;
